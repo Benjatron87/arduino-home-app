db2eb33982cc9ba333113ae11ffc59716260175d
Benjatron <benjt122@gmail.com>
2019-04-02 21:39:41 -0400

    small change

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+app.get("/api/temp", (req, res)=> {
+    res.json();
+})
+
+app.post("/api/temp", (req, res)=> {
+    res.json();
+})
+
 app.get("/api/led/", (req, res) => {
    
     db.led.findAll({})
