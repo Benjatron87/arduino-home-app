39ea236c86c5f614863ca111380cbeda29262a39
Benjatron <benjt122@gmail.com>
2019-05-31 16:01:06 -0400

    Added a pretty import line to help run app on heroku

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -67,8 +67,4 @@ module.exports = function(app) {
           });
       });
 
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/index.html"));
-    });
-
 };
--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use('/', authRoutes)
 
 const PORT = process.env.PORT ||  4000;
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  });
+
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 })
